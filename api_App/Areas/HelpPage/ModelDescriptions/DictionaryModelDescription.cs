10a5f46418ab00d991798dbe6e361bcb19d3dce8
ELHOUCINE <houcine.mariami1995@gmail.com>
2020-06-23 20:50:20 +0100

    Ajoutez des fichiers projet.

---
+++ api_App/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api_App.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ api_App/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api_App.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
